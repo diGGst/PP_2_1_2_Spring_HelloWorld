9db35e222f7a5428b46e1ba7f7b2fd120919e347
Вячеслав <smiv1995@gmail.com>
2022-05-04 23:33:20 +0300

    Task done, ready for check

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,14 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld helloWorld = applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld helloWorld2 = applicationContext.getBean("helloworld", HelloWorld.class);
+
+        Cat cat = applicationContext.getBean("cat", Cat.class);
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println(helloWorld == helloWorld2);
+        System.out.println(cat == cat2);
     }
 }
